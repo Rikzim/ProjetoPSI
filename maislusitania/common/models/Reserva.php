d3b04b3612740e2fd520070b10623cda57c954ce
Henrik Dambros <2241589@my.ipleiria.pt>
2026-01-05 18:42:55 +0000

    outra porta

--- maislusitania/common/models/Reserva.php
+++ maislusitania/common/models/Reserva.php
@@ -356,7 +356,7 @@ class Reserva extends \yii\db\ActiveRecord
     public function FazPublishNoMosquitto($canal, $msg)
     {
         $server = "172.22.21.218"; 
-        $port = 8080;
+        $port = 443;
         $username = ""; // set your username
         $password = ""; // set your password
         $client_id = "phpMQTT-publisher"; // unique!
